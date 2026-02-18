8131139c453b560a14f02edd94be75ffad91960c
taki <tarassevcuk700@gmail.com>
2026-02-18 19:29:19 +0200

    .

--- src/js/api/getEvents.js
+++ src/js/api/getEvents.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://app.ticketmaster.com/discovery/v2";
 const API_KEY = "BIqpeJSCCVibv6jIhfaVoFVpuL0cSADG";
 
 // приклад запиту
-// https://app.ticketmaster.com/discovery/v2/events.json?apikey=BIqpeJSCCVibv6jIhfaVoFVpuL0cSADG
+// https://app.ticketmaster.com/discovery/v2/events?apikey=BIqpeJSCCVibv6jIhfaVoFVpuL0cSADG
 
 // /events – пошук та отримання подій
 // /events/{id} – деталі конкретної події
@@ -11,3 +11,10 @@ const API_KEY = "BIqpeJSCCVibv6jIhfaVoFVpuL0cSADG";
 // /venues – місця проведення
 // /suggest – автопідказки для пошуку
 // /classifications – жанри, сегменти, типи
+
+const getEvents = async () => {
+  const res = await fetch(`${BASE_URL}/events?apikey=${API_KEY}`);
+  return res.json();
+};
+
+export { getEvents };
